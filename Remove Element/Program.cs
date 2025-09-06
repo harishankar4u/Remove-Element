02e31aa0dab7c877a04914f8304d2fe19a99d1f4
Hari Shankar Singh <hari@lmg8.tech>
2025-09-06 17:27:44 +0530

    Add project files.

---
+++ Remove Element/Program.cs
@@ -0,0 +1,23 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(RemoveElement([0, 1, 2, 2, 3, 0, 4, 2],2)); //output 5,  nums = [0,1,4,0,3,_,_,_]
+        Console.WriteLine(RemoveElement([3, 2, 2, 3],3)); //output 2, nums = [2,2,_,_]
+    }
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int count = 0;  // position for next valid element
+        for (int i = 0; i < nums.Length; i++)
+        {
+           if (nums[i] != val)
+           {
+
+                //You must copy valid elements forward (nums[j] = nums[i]), not just count them.
+                nums[count] = nums[i];
+                count++;
+           }
+        }
+        return count;
+    }
+}
